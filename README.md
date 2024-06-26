66733a298e5b4d9b310bc47d7f6d577e8a3e5058
Muhammad540 <thisisdeahmed@gmail.com>
2024-06-26 20:22:07 +0300

    added xacro files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Differential Drive Robot Assignment
+This repo contains an introduction to URDF for ME461 Students, where they will create their own simple differential drive robots. 
---
+++ src/rabbit_description/launch/launch_simulation.launch.py
@@ -0,0 +1,47 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name = 'rabbit_description'
+
+    # Process the URDF file
+    pkg_path = os.path.join(get_package_share_directory(package_name))
+    xacro_file = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro')
+    robot_description_config = Command(['xacro ', xacro_file])
+    
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description_config, 'use_sim_time': True}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+    
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+    )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description', '-entity', 'rabbit'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        node_robot_state_publisher,
+        gazebo,
+        spawn_entity
+    ])
